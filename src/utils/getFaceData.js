d50169fe97b1ab4d1463cdfcc3ca944103c07760
Jose Arevalo <arevaj01@stepstone.com>
2020-02-24 11:20:23 -0600

    Change MS face API to Google Vision API

--- src/utils/getFaceData.js
+++ src/utils/getFaceData.js
@@ -1,26 +1,32 @@
-const {faceAPI} = require("../../package.json");
+const {googleVisionAPI} = require("../../package.json");
 const fetch = require('node-fetch'); 
-const endPointParams = {
-    returnFaceId : false,
-    returnFaceLandmarks : false,
-};
-const metadata =  {
-    'Content-Type' : "application/octet-stream",
-    'Ocp-Apim-Subscription-Key' : faceAPI.key
-};
+const url = googleVisionAPI.endpoint + googleVisionAPI.key;
+
 const callFaceApi = (binaryImage) => {
-    const header = new fetch.Headers(metadata);
-    const init = { 
-        method: 'POST',
-        headers: header,
-        body: binaryImage
+
+    const imageObj = {
+        image: {
+            content: binaryImage
+        },
+        features: [{
+            type: "FACE_DETECTION",
+            maxResults: 5
+        }]
     };
-    const urlparams = Object.entries(endPointParams).map(([key, val]) => `${key}=${val}`).join('&');
-    const url = faceAPI.endpoint + urlparams;
-    const apiCall = fetch(url,init).then(res =>{
-        const a = res;
+
+    const requestBody = {
+        requests: [
+            imageObj
+        ]
+    };
+
+    const apiCall = fetch(url, {method: "POST", body: JSON.stringify(requestBody)})
+    .then(res => res.json())
+    .then(json => {
+        console.log(json)
         debugger;
-    }).catch(console.error);
+    })
+    .catch(console.error);
 };
 
 module.exports = {callFaceApi};
