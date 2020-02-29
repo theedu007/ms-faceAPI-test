8d063bb13b70a793db3d0912cc3698cd2e35d82a
theedu007 <eabigboy4@gmail.com>
2020-02-28 22:53:08 -0600

    Show src image in bowser

--- src/public/js/getImage.js
+++ src/public/js/getImage.js
@@ -11,7 +11,34 @@ const getImage = () => {
     }).then(blobResponse => {
         const imgHtml = document.createElement("img");
         imgHtml.src = URL.createObjectURL(blobResponse);
-        document.querySelector("#imageDiv").appendChild(imgHtml);
+        const imageContainer = document.querySelector("#scanImage");
+        imageContainer.innerHTML = "";
+        imageContainer.appendChild(imgHtml);
+        
     })
     .catch(console.error());
+}
+
+const addSrcImage = () => {
+    const file = document.querySelector("input[type=file]").files[0];
+    const srcDiv = document.querySelector("#srcImage");
+    const imgHtml = document.createElement("img");
+    const reader = new FileReader();
+    reader.onload = () => {
+        imgHtml.src = reader.result;
+
+    };
+    if(file){
+        reader.readAsDataURL(file);
+        srcDiv.innerHTML = "";
+        srcDiv.appendChild(imgHtml);
+    }
+    else{
+        srcDiv.innerHTML = "";
+    }
+}
+
+window.onload = () => {
+    const fileInput = document.querySelector("input[type=file]");
+    fileInput.addEventListener("change", addSrcImage);
 }
